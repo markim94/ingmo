e6dad7b48e8cdc52684dc719d6e78a0f2f16de95
markim94 <markim94@naver.com>
2019-01-24 18:17:46 +0900

    add intro_activity

---
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.tistory.markim94.android_ingmo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.intro_activity)
+    }
+}
