2bf1ca978e357d00eb4a080b2935092b01f7a5bb
markim94 <markim94@naver.com>
2019-01-30 18:16:19 +0900

    add Basicinfo.java for define activity request code

--- android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoListActivity.java
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoListActivity.java
@@ -2,12 +2,20 @@ package com.tistory.markim94.android_ingmo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MemoListActivity extends AppCompatActivity {
+
+    Button btnAddMemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memo_list_activity);
+
+        btnAddMemo = findViewById(R.id.btnAddMemo);
+
+
     }
 }
