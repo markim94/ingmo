7bbbef4647be42882d9979f339a25b6400fa9868
markim94 <markim94@naver.com>
2019-01-30 18:31:22 +0900

    add layout_memoInsertActivity

--- android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/BasicInfo.java
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/BasicInfo.java
@@ -3,6 +3,7 @@ package com.tistory.markim94.android_ingmo;
 public class BasicInfo {
 
     //========== 액티비티 요청 코드  ==========//
+    public static final int REQ_INSERT_ACTIVITY = 1002;
 //    public static final int REQ_VIEW_ACTIVITY = 1001;
 //    public static final int REQ_INSERT_ACTIVITY = 1002;
 //    public static final int REQ_PHOTO_CAPTURE_ACTIVITY = 1501;
--- android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoListActivity.java
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoListActivity.java
@@ -1,20 +1,36 @@
 package com.tistory.markim94.android_ingmo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MemoListActivity extends AppCompatActivity {
 
     Button btnAddMemo;
+    ListView listMemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memo_list_activity);
 
+        listMemo = findViewById(R.id.listMemo);
         btnAddMemo = findViewById(R.id.btnAddMemo);
+
+
+        btnAddMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MemoInsertActivity.class);
+                //intent.putExtra(BasicInfo.KEY_MEMO_MODE, BasicInfo.MODE_INSERT);
+                startActivityForResult(intent, BasicInfo.REQ_INSERT_ACTIVITY);
+            }
+        });
 
 
     }
