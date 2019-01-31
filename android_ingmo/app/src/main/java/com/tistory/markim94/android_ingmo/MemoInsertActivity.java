d21dc350d9ae63b728fab1208fc3239a29650c50
markim94 <markim94@naver.com>
2019-01-31 15:53:28 +0900

    modify README.md & add memoinsertactivity.class

--- android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoInsertActivity.java
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoInsertActivity.java
@@ -2,15 +2,59 @@ package com.tistory.markim94.android_ingmo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MemoInsertActivity extends AppCompatActivity {
+
+    Button btnStatusMemo;
+    Button btnStoreMemo;
+    Button btnDeleteMemo;
+    Button btnCancelMemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.memo_insert_activity);
 
+        // 타이틀 상태버튼과 하단부의 버튼 객체 참조 메소드.
+        buttonLayout();
+
 
 
     }
+
+
+    /**
+     * 상단 상태버튼, 하단부 버튼 객체 참조 메소드
+     */
+    void buttonLayout(){
+        btnStatusMemo = findViewById(R.id.btnStatusMemo);
+        btnStoreMemo = findViewById(R.id.btnStoreMemo);
+        btnDeleteMemo = findViewById(R.id.btnDeleteMemo);
+        btnCancelMemo = findViewById(R.id.btnCancelMemo);
+
+        btnCancelMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 실행코드
+            }
+        });
+
+        btnDeleteMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 실행코드
+            }
+        });
+
+        btnStoreMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 실행코드
+            }
+        });
+
+    }
+
 }
