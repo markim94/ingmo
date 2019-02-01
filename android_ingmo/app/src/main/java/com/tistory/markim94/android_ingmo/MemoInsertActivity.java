5ed4792620904918c0e12da017f3bdb8ad2ad4e0
markim94 <markim94@naver.com>
2019-02-01 13:16:14 +0900

    add processIntent for update memo data

--- android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoInsertActivity.java
+++ android_ingmo/app/src/main/java/com/tistory/markim94/android_ingmo/MemoInsertActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /*
  * create by brother jun
@@ -17,12 +18,16 @@ public class MemoInsertActivity extends AppCompatActivity {
     Button btnStoreMemo;
     Button btnDeleteMemo;
     Button btnCancelMemo;
+    Button btnStarMemo;
 
     // intent를 통해 넘겨받은 memo data를 저장할 String 객체
     String mMemoId;
     String mMemoDate;
-    String mMemoText;
-    String isStarChecked;
+    EditText mMemoText;
+    String mMemoStarChecked;
+
+    // 중요 메모인지 아닌지
+    boolean isChecked;
 
 
     // 인텐트로 전달된 메모의 모드를 저장할 String
@@ -49,10 +54,14 @@ public class MemoInsertActivity extends AppCompatActivity {
 
             btnStatusMemo.setText("메모 보기");
             btnDeleteMemo.setVisibility(View.VISIBLE);
+            btnStoreMemo.setText("수정");
+
             // 이외의 경우 새로운 메모 추가 버튼을 누른 경우 이므로
         } else {
             btnStatusMemo.setText("새 메모");
+            btnStoreMemo.setText("저장");
             btnDeleteMemo.setVisibility(View.GONE);
+
         }
 
     }
@@ -61,9 +70,22 @@ public class MemoInsertActivity extends AppCompatActivity {
     public void processIntent(Intent intent){
         mMemoId = intent.getStringExtra("MEMO_ID");
         mMemoDate = intent.getStringExtra("MEMO_DATE");
-        mMemoText = intent.getStringExtra("MEMO_TEXT");
-        isStarChecked = intent.getStringExtra("MEMO_STARCHECKED");
-        
+        mMemoText.setText(intent.getStringExtra("MEMO_TEXT"));
+        mMemoStarChecked = intent.getStringExtra("MEMO_STARCHECKED");
+
+        setStatusMemo(mMemoDate,mMemoStarChecked);
+
+    }
+
+
+    public void setStatusMemo(String mMemoDate, String mMemoStarChecked){
+        if (mMemoStarChecked.equals("true")){
+            isChecked = true;
+            btnStarMemo.setBackgroundResource(R.drawable.btn_star);
+        } else {
+            isChecked = false;
+            btnStarMemo.setBackgroundResource(R.drawable.btn_non_star);
+        }
     }
 
 
@@ -75,10 +97,11 @@ public class MemoInsertActivity extends AppCompatActivity {
      * 상단 상태버튼, 하단부 버튼 객체 참조 메소드
      */
     void buttonLayout(){
-        btnStatusMemo = findViewById(R.id.btnStatusMemo);
-        btnStoreMemo = findViewById(R.id.btnStoreMemo);
-        btnDeleteMemo = findViewById(R.id.btnDeleteMemo);
-        btnCancelMemo = findViewById(R.id.btnCancelMemo);
+        btnStatusMemo = (Button)findViewById(R.id.btnStatusMemo);
+        btnStoreMemo = (Button)findViewById(R.id.btnStoreMemo);
+        btnDeleteMemo = (Button)findViewById(R.id.btnDeleteMemo);
+        btnCancelMemo = (Button)findViewById(R.id.btnCancelMemo);
+        btnStarMemo = (Button)findViewById(R.id.btnStarMemo);
 
         btnCancelMemo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,11 +113,18 @@ public class MemoInsertActivity extends AppCompatActivity {
         btnDeleteMemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 실행코드
+                // dialog를 띄운후, 삭제 누를시
             }
         });
 
         btnStoreMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 실행코드
+            }
+        });
+
+        btnStarMemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 실행코드
